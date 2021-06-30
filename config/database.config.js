ff5e975b8d998a394f604ab01b81879e51700fdb
pranit390 <51707022+pranit390@users.noreply.github.com>
2021-06-30 15:09:24 +0530

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -2,9 +2,3 @@ const mongoose=require('mongoose');
 mongoose.connect('mongodb://localhost/book', { useNewUrlParser: true })
 
 
-// module.exports = {
-
-   
-
-//     url: 'mongodb://localhost:27017/book'
-// }
